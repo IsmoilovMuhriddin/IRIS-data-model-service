644fb4f98efe956cdf2848816ca985b5b0893965
Muhriddin Ismoilov <ismoilovmuh1996@gmail.com>
2018-09-26 23:35:04 +0500

    pep8 refactoring

--- app/app.py
+++ app/app.py
@@ -1,56 +1,59 @@
-
+""" Flask app for client configuation of routes """
 import json
-from flask import Flask, request , send_file
-from flask import render_template, make_response
+from flask import Flask, request, send_file
+from flask import render_template
 import tasks
 APP = Flask(__name__)
 
 @APP.route('/')
 def index():
+    """ index page of Client web app """
     return render_template('index.html')
 
 
-@APP.route('/predict', methods = ['POST'])
+@APP.route('/predict', methods=['POST'])
 def predict():
+    """ predicts route - creates new task and waits """
     data = request.form.to_dict()
     data.pop('Submit')
-    print(request.form)
     job = tasks.get_prediction.delay(data)
     return render_template('home.html', JOBID=job.id)
 
 
 @APP.route('/result')
 def result():
+    """ result route - return pdf result when ready """
     jobid = request.values.get('jobid')
     if jobid:
         job = tasks.get_job(jobid)
         pdf_output = job.get()
-        return send_file('./' + pdf_output,attachment_filename=str(jobid)+'.pdf')
-    else:
-        return 404
+        return send_file('./' + pdf_output, attachment_filename=str(jobid)+'.pdf')
+    return 404
 
 
 @APP.route('/progress')
 def progress():
+    """ progress route - checks the progress of given task id """
     jobid = request.values.get('jobid')
+    return_result = '{}'
     if jobid:
         job = tasks.get_job(jobid)
         if job.state == 'PROGRESS':
-            return json.dumps(dict(
+            return_result = json.dumps(dict(
                 state=job.state,
                 progress=job.result['current'],
             ))
         elif job.state == 'SUCCESS':
-            return json.dumps(dict(
+            return_result = json.dumps(dict(
                 state=job.state,
                 progress=1.0,
             ))
         elif job.state == 'FAILURE':
-            return json.dumps(dict(
+            return_result = json.dumps(dict(
                 state=job.state,
                 progress=1.0,
             ))
-    return '{}'
+    return return_result
 
 
 
--- app/tasks.py
+++ app/tasks.py
@@ -1,19 +1,15 @@
-import time
-import random
-import datetime
-from flask import jsonify
+""" Celery Tasks for the predicting and result progress"""
 from celery import Celery, current_task
 from celery.result import AsyncResult
 from celery.utils.log import get_task_logger
 from sklearn.externals import joblib
 from fpdf import FPDF
-import json
 
-model_filename = 'finalized_model.sav'
-loaded_model = joblib.load(model_filename)
+MODEL_FILENAME = 'finalized_model.sav'
+LOADED_MODEL = joblib.load(MODEL_FILENAME)
 REDIS_URL = 'redis://redis:6379/0'
 BROKER_URL = 'amqp://admin:mypass@rabbit//'
-logger = get_task_logger(__name__)
+LOGGER = get_task_logger(__name__)
 CELERY = Celery('tasks',
                 backend=REDIS_URL,
                 broker=BROKER_URL)
@@ -22,58 +18,45 @@ CELERY = Celery('tasks',
 CELERY.conf.accept_content = ['json', 'msgpack']
 CELERY.conf.result_serializer = 'msgpack'
 
-
 def get_job(job_id):
+    """ returns task of the given id"""
     return AsyncResult(job_id, app=CELERY)
 
-
-"""
-data = {
-    "Sepal length" : "4.6",
-    "Sepal width" : "3.1",
-    "Petal length" : "1.5",
-    "Petal width" : "0.2"
-}
-"""
-keys = ['Sepal length', 'Sepal width', 'Petal length','Petal width']
+KEYS = ['Sepal length', 'Sepal width', 'Petal length', 'Petal width']
 @CELERY.task()
 def get_prediction(data):
+    """ input sample data input- predicts result and creates pdf """
     jobid = current_task.request.id
-    current_task.update_state(state='PROGRESS',meta = {'current':0.1})
-    logger.info('JobId: {0}, Status: PROGRESS, Meta: prediction started'.format(jobid))
+    current_task.update_state(state='PROGRESS', meta={'current':0.1})
+    LOGGER.info('JobId: %s, Status: PROGRESS, Meta: prediction started', jobid)
 
     verified_data = True
-    for i in keys:
-        verified_data = verified_data and (i in data) 
-    logger.info('JobId: {0}, Status: PROGRESS, Meta: data verified = {1}'.format(jobid,data))
-    
+    for i in KEYS:
+        verified_data = verified_data and (i in data)
+    LOGGER.info('JobId: %s, Status: PROGRESS, Meta: data verified', jobid)
     current_task.update_state(state='PROGRESS', meta={'current': 0.2})
     if verified_data:
-        result = loaded_model.predict([[float(data[x]) for x in keys]])
-        logger.info('JobId: {0}, Status: PROGRESS, Meta: model predicted'.format(jobid))
-    
+        result = LOADED_MODEL.predict([[float(data[x]) for x in KEYS]])
+        LOGGER.info('JobId: {%s}, Status: PROGRESS, Meta: model predicted = %s ', jobid, result)
         string_result = ''
         current_task.update_state(state='PROGRESS', meta={'current': 0.6})
         pdf = FPDF()
         pdf.add_page()
         pdf.set_font("Arial", size=12)
         line = 1
-        logger.info('JobId: {0}, Status: PROGRESS, Meta: starting to write to pdf'.format(jobid))
-    
-        for k, v in data.items():
-            data_line = k + ' = ' + str(v)
+        LOGGER.info('JobId: %s, Status: PROGRESS, Meta: starting to write to pdf', jobid)
+        for key, val in data.items():
+            data_line = key + ' = ' + str(val)
             string_result += data_line + '\n'
             pdf.cell(50, 10, txt=data_line, ln=line, align="C")
             line += 1
-        
         current_task.update_state(state='PROGRESS', meta={'current': 0.8})
-        
         pdf.cell(50, 10, txt='Predicted:' + result[0], ln=line, align="C")
         result_filename = 'results_data/'+str(jobid)+'.pdf'
         pdf.output(result_filename)
-        logger.info('JobId: {0}, Status: PROGRESS, Meta: written to pdf'.format(jobid))
+        LOGGER.info('JobId: %s, Status: PROGRESS, Meta: written to pdf', jobid)
         current_task.update_state(state='PROGRESS', meta={'current': 0.9})
         return result_filename
     current_task.update_state(state='FAILURE', meta={'current': 1})
-    logger.info('JobId: {0}, Status: FAILURE, Meta: data format is wrong'.format(jobid))
+    LOGGER.info('JobId: %s, Status: FAILURE, Meta: data format is wrong', jobid)
     return "error: data type is wrong"
